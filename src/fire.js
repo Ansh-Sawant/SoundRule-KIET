2eb0fb93650b652fdd7d571cf6e08549529d24c4
Ansh Sawant <63812064+Ansh-Sawant@users.noreply.github.com>
2021-06-09 14:47:48 +0530

    Update fire.js
    Important Details Removed

--- src/fire.js
+++ src/fire.js
@@ -4,14 +4,14 @@ import "firebase/storage";
 // THIS COMPONENT IS FOR DETAILS OF SOUNDRULE PROJECT IN FIREBASE
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDhcWBkUmxVg5VkwNRCZYIp_SYZ3Sk9dtc",
-  authDomain: "soundrule.firebaseapp.com",
-  databaseURL: "https://soundrule.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
   projectId: "soundrule",
-  storageBucket: "soundrule.appspot.com",
-  messagingSenderId: "718907645622",
-  appId: "1:718907645622:web:f6fde78f15b470896b5017",
-  measurementId: "G-SNW031C2SK",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
